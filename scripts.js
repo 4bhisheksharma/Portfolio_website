7bf9ba4ea0a4a6c10b4540d6c472c012e38800b1
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2024-12-04 15:13:00 +0545

    updated navigations

--- scripts.js
+++ scripts.js
@@ -19,31 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const header = document.querySelector('header');
-//     let timeout = null;
-
-//     // Function to hide header after 3 seconds
-//     const hideHeader = () => {
-//         header.style.transform = 'translateY(-100%)'; // Move header above viewport
-//     };
-
-//     // Reset timeout and show header on mousemove or click
-//     const resetTimer = () => {
-//         clearTimeout(timeout);
-//         header.style.transform = 'translateY(0)';
-//         timeout = setTimeout(hideHeader, 2000); // 3000ms = 3 seconds
-//     };
-
-//     // Initial setup: Show header and start timer
-//     resetTimer();
-
-//     // Reset timer on mousemove or click
-//     document.addEventListener('mousemove', resetTimer);
-//     document.addEventListener('click', resetTimer);
-//     document.addEventListener('touchstart', resetTimer);
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     const hours = new Date().getHours();
     let greeting;
